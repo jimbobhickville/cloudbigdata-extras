a871c394f1c701bd87561eec87fbf07e35655614
David Dobbins <david.dobbins@rackspace.com>
2015-03-02 18:25:06 -0600

    add license

--- examples/LanguageCorrelationSparkMllib/github_lang_correlations.py
+++ examples/LanguageCorrelationSparkMllib/github_lang_correlations.py
@@ -1,3 +1,21 @@
+# Copyright Rackspace Inc.
+# All Rights Reserved
+#
+#
+# The code in this project is made available as free and open source software under the terms and
+# conditions of the GNU Public License. For more information, please refer to the LICENSE text file included with this project,
+# or visit gnu.org if the license file was not included.
+# This SOFTWARE PRODUCT is provided by THE PROVIDER "as is" and "with all faults."
+# THE PROVIDER makes no representations or warranties of any kind concerning the
+# safety, suitability, lack of viruses, inaccuracies, typographical errors, or
+# other harmful components of this SOFTWARE PRODUCT. There are inherent dangers
+# in the use of any software, and you are solely responsible for determining
+# whether this SOFTWARE PRODUCT is compatible with your equipment and other
+# software installed on your equipment. You are also solely responsible for the
+# protection of your equipment and backup of your data, and THE PROVIDER will
+# not be liable for any damages you may suffer in connection with using,
+# modifying, or distributing this SOFTWARE PRODUCT.
+
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
